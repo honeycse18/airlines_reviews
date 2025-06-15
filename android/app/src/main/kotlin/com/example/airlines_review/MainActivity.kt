12701db285f590433ad32ab49b3032d0163b9a81
honeycse472 <honeycse472@gmail.com>
2025-06-15 20:05:54 +0600

    Upload project foundation

---
+++ android/app/src/main/kotlin/com/example/airlines_review/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.airlines_review
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Home());
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Airlines Review")),
+      body: Center(child: Text("Airlines Review")),
+    );
+  }
+}
