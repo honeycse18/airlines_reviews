481b195a18c36389666bb51aa8512c8bbb933aa4
honeycse472 <honeycse472@gmail.com>
2025-06-17 02:34:11 +0600

    Completed appbar and customscrollview

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:airlines_review/utils/constant/colors.dart';
+import 'package:airlines_review/utils/constant/images.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -12,9 +14,25 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.white,
         title: Text("Airlines Review"),
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-          CircleAvatar(child: Icon(Icons.person)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+          SvgPicture.asset(AppAssetImages.notificationLogoLine),
+          SizedBox(width: 24),
+          CircleAvatar(
+            radius: 17.41,
+            child: Center(
+              child: Container(
+                height: 34.82,
+                width: 34.82,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.black, width: 2),
+                ),
+
+                child: Center(child: Icon(Icons.person)),
+              ),
+            ),
+          ),
+          SizedBox(width: 18),
+          IconButton(onPressed: () {}, icon: Icon(Icons.menu, size: 32)),
         ],
       ),
       body: CustomScrollView(
---
+++ lib/utils/constant/images.dart
@@ -0,0 +1,19 @@
+class AppAssetImages {
+  static const String _iconAssetsLocation = 'assets/icons/';
+  static const String _lineIconAssetsLocation = '${_iconAssetsLocation}line/';
+  static const String _solidIconAssetsLocation = '${_iconAssetsLocation}solid/';
+  static const String _imageAssetsLocation = 'assets/images/';
+
+  /* <---- Lines ----> */
+
+  static const String notificationLogoLine =
+      '${_lineIconAssetsLocation}notification.svg';
+
+  /* <---- Solid ----> */
+  static const String sent = '${_solidIconAssetsLocation}sent.svg';
+
+  /*<--------Images-------->*/
+
+  static const String splashBackgroundImage =
+      '${_imageAssetsLocation}splash.png';
+}
